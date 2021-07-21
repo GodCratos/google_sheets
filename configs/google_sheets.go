4e4a07f9c4b23bd3eeaea7784f38dfcb31dfd9fa
GodCratos <79117972+GodCratos@users.noreply.github.com>
2021-07-21 11:08:07 +0300

    Update google_sheets.go

--- configs/google_sheets.go
+++ configs/google_sheets.go
@@ -1,9 +1,9 @@
 package configs
 
 func GoogleSheetsGetSheetsID() string {
-	return "1RzNKIyfJgiY0n8cnafya2OPB4D1ETshppZuikC2ob4Q"
+	return ""
 }
 
 func GoogleSheetsGetSheetsName() string {
-	return "sheets1"
+	return ""
 }
